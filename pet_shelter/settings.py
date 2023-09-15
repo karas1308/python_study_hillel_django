c012ccc8ee6ab4c561f4dd49c52b360ce27c3bc1
ihor.shchuka <ihor.shchuka@gmail.com>
2023-09-15 15:32:59 +0300

    Fixed lint errors

--- pet_shelter/settings.py
+++ pet_shelter/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-_d$1w^**&z#toj0r3figxvu7j18q4&$uxehv^ss4$m6adpx1xz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
