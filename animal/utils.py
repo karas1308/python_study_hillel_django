543d1ac86e908c54f652c1217784441935ac2188
ihor.shchuka <ihor.shchuka@gmail.com>
2023-09-28 16:16:21 +0300

    Added HW Mock

---
+++ animal/utils.py
@@ -0,0 +1,36 @@
+from animal.time_scheduler import calculate_booking_time
+
+
+def calculate_free_time(booked_time_frames, min_time_duration, min_time_slot=15):
+    return __calculate_free_time(booked_time_frames=booked_time_frames, min_time_duration=min_time_duration,
+                                 min_time_slot=min_time_slot)
+
+
+def __calculate_free_time(booked_time_frames, min_time_duration, min_time_slot=15):
+    return calculate_booking_time(booked_time_frames=booked_time_frames, min_time_duration=min_time_duration,
+                                  min_time_slot=min_time_slot)
+
+
+class CalculateFreeTime:
+
+    def calculate_free_time_1(self, booked_time_frames, min_time_duration, min_time_slot=15):
+        return calculate_booking_time(booked_time_frames=booked_time_frames, min_time_duration=min_time_duration,
+                                      min_time_slot=min_time_slot)
+
+    @classmethod
+    def calculate_free_time_2(cls, booked_time_frames, min_time_duration, min_time_slot=15):
+        return calculate_booking_time(booked_time_frames=booked_time_frames, min_time_duration=min_time_duration,
+                                      min_time_slot=min_time_slot)
+
+    @staticmethod
+    def calculate_free_time_3(booked_time_frames, min_time_duration, min_time_slot=15):
+        return calculate_booking_time(booked_time_frames=booked_time_frames, min_time_duration=min_time_duration,
+                                      min_time_slot=min_time_slot)
+
+
+def calc_proxy(*args, **kwargs):
+    c_f_t = CalculateFreeTime()
+    return c_f_t.calculate_free_time_1(*args, **kwargs)
+
+
+calc_free_time = CalculateFreeTime()
---
+++ tests/unittest_bases/tests_mock.py
@@ -0,0 +1,114 @@
+import datetime
+import unittest
+from unittest.mock import Mock, patch
+
+from animal.time_scheduler import calculate_booking_time
+from animal.utils import CalculateFreeTime, calc_free_time, calc_proxy, calculate_free_time
+
+
+def custom_mock(*args, **kwargs):
+    return [1]
+
+
+class ScheduleMock:
+    def calculate_free_time_1(self, *args, **kwargs):
+        return [1]
+
+    @classmethod
+    def calculate_free_time_2(cls, *args, **kwargs):
+        return [1]
+
+    @staticmethod
+    def calculate_free_time_3(*args, **kwargs):
+        return [1]
+
+
+class TestScheduleMock(unittest.TestCase):
+    @patch('tests_mock.calculate_booking_time', Mock(return_value=[1]))
+    def test_schedule_mock(self):
+        expected_result = []
+        booked_time_frames = [(datetime.datetime(2023, 8, 1, 8, 0),
+                               datetime.datetime(2023, 8, 1, 18, 0))]
+        min_time_duration = 1
+        min_time_slot = 300
+        actual_result = calculate_booking_time(booked_time_frames, min_time_duration, min_time_slot)
+        print(f"expected_result:{expected_result} vs actual_result:{actual_result}")
+        self.assertEqual(expected_result, actual_result)
+
+    @patch('animal.utils.calculate_booking_time', Mock(return_value=[1]))
+    def test_schedule_mock_1(self):
+        expected_result = []
+        booked_time_frames = [(datetime.datetime(2023, 8, 1, 8, 0),
+                               datetime.datetime(2023, 8, 1, 18, 0))]
+        min_time_duration = 1
+        min_time_slot = 300
+        actual_result = calculate_free_time(booked_time_frames, min_time_duration, min_time_slot)
+        print(f"expected_result:{expected_result} vs actual_result:{actual_result}")
+        self.assertEqual(expected_result, actual_result)
+
+    @patch('animal.utils.calculate_booking_time', custom_mock)
+    def test_schedule_mock_2(self):
+        expected_result = []
+        booked_time_frames = [(datetime.datetime(2023, 8, 1, 8, 0),
+                               datetime.datetime(2023, 8, 1, 18, 0))]
+        min_time_duration = 1
+        min_time_slot = 300
+        actual_result = calculate_free_time(booked_time_frames, min_time_duration, min_time_slot)
+        print(f"expected_result:{expected_result} vs actual_result:{actual_result}")
+        self.assertEqual(expected_result, actual_result)
+
+    @patch('tests_mock.CalculateFreeTime', ScheduleMock)
+    def test_schedule_mock_3(self):
+        expected_result = []
+        booked_time_frames = [(datetime.datetime(2023, 8, 1, 8, 0),
+                               datetime.datetime(2023, 8, 1, 18, 0))]
+        min_time_duration = 1
+        min_time_slot = 300
+        calculate_time = CalculateFreeTime()
+        actual_result = calculate_time.calculate_free_time_1(booked_time_frames, min_time_duration, min_time_slot)
+        print(f"expected_result:{expected_result} vs actual_result:{actual_result}")
+        self.assertEqual(expected_result, actual_result)
+
+    @patch('tests_mock.CalculateFreeTime', ScheduleMock)
+    def test_schedule_mock_4(self):
+        expected_result = []
+        booked_time_frames = [(datetime.datetime(2023, 8, 1, 8, 0),
+                               datetime.datetime(2023, 8, 1, 18, 0))]
+        min_time_duration = 1
+        min_time_slot = 300
+        actual_result = CalculateFreeTime.calculate_free_time_2(booked_time_frames, min_time_duration, min_time_slot)
+        print(f"expected_result:{expected_result} vs actual_result:{actual_result}")
+        self.assertEqual(expected_result, actual_result)
+
+    @patch('tests_mock.CalculateFreeTime', ScheduleMock)
+    def test_schedule_mock_5(self):
+        expected_result = []
+        booked_time_frames = [(datetime.datetime(2023, 8, 1, 8, 0),
+                               datetime.datetime(2023, 8, 1, 18, 0))]
+        min_time_duration = 1
+        min_time_slot = 300
+        actual_result = CalculateFreeTime.calculate_free_time_3(booked_time_frames, min_time_duration, min_time_slot)
+        print(f"expected_result:{expected_result} vs actual_result:{actual_result}")
+        self.assertEqual(expected_result, actual_result)
+
+    @patch('animal.utils.CalculateFreeTime', ScheduleMock)
+    def test_schedule_mock_6(self):
+        expected_result = []
+        booked_time_frames = [(datetime.datetime(2023, 8, 1, 8, 0),
+                               datetime.datetime(2023, 8, 1, 18, 0))]
+        min_time_duration = 1
+        min_time_slot = 300
+        actual_result = calc_proxy(booked_time_frames, min_time_duration, min_time_slot)
+        print(f"expected_result:{expected_result} vs actual_result:{actual_result}")
+        self.assertEqual(expected_result, actual_result)
+
+    @patch('animal.utils.calc_free_time', ScheduleMock)
+    def test_schedule_mock_7(self):
+        expected_result = []
+        booked_time_frames = [(datetime.datetime(2023, 8, 1, 8, 0),
+                               datetime.datetime(2023, 8, 1, 18, 0))]
+        min_time_duration = 1
+        min_time_slot = 300
+        actual_result = calc_free_time(booked_time_frames, min_time_duration, min_time_slot)
+        print(f"expected_result:{expected_result} vs actual_result:{actual_result}")
+        self.assertEqual(expected_result, actual_result)
